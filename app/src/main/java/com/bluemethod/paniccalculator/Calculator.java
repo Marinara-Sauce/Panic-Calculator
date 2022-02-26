8e340d07b00d652f73bcdb18d66793f3545d617b
Dan Bliss <danbliss14027@gmail.com>
2022-02-25 21:43:45 -0500

    Created the calculator controller

--- app/src/main/java/com/bluemethod/paniccalculator/Calculator.java
+++ app/src/main/java/com/bluemethod/paniccalculator/Calculator.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Activity for the main calculator view
@@ -21,10 +24,16 @@ public class Calculator extends AppCompatActivity {
 
     //TODO: Modify the UI in activity_calculator.xml
 
+    //Used for triggering the SOS, is set to false after .25 seconds of being hit
+    private boolean equalsTapped = false;
+    Timer equalsTimer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        CalculatorFunctionality calculator = new CalculatorFunctionality();
 
         //Begin collecting scene information and init-ing variables
 
@@ -51,8 +60,167 @@ public class Calculator extends AppCompatActivity {
         Button equalsButton = (Button) findViewById(R.id.bEqualsButton);
 
         //Clear Button
-        Button button = (Button) findViewById(R.id.bClearButton);
+        Button clearButton = (Button) findViewById(R.id.bClearButton);
 
-        //TODO: Set on click listeners
+        //---- ON CLICK LISTENERS ----//
+
+        oneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        twoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        threeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        fourButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        fiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        sixButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        sevenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        eightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        nineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        zeroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        timesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        divideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        equalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Not only should this solve, but we should also check for double presses
+                //and dispatch the SOS
+
+                if (equalsTapped)
+                    sendSOS(view);
+
+                equalsTapped = true;
+
+                //Deactivates the equals primer after .25 seconds
+                equalsTimer = new Timer();
+                equalsTimer.schedule(new RemindTask(), 250);
+
+                //TODO: Solve the equation
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    /**
+     * Class used to manage the double tapping for the equals button
+     */
+    class RemindTask extends TimerTask
+    {
+        public void run()
+        {
+            equalsTapped = false;
+        }
+    }
+
+    /**
+     * Updates the calculator's equation display
+     *
+     * @param view the text view to update
+     * @param calculator the calculator class
+     */
+    private void updateDisplay(TextView view, CalculatorFunctionality calculator)
+    {
+        view.setText(calculator.getDisplay());
+    }
+
+    /**
+     * Sends an SOS Signal
+     *
+     * @param view temp param for the SOS placeholder
+     */
+    private void sendSOS(View view)
+    {
+        //TODO: Dispatch SOS Signal
+        Snackbar.make(view, "SOS Activated!! Wee Wooo Wee Woo", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
