da81c6a122e99b08cdc38ca4cad2ac40616e78a4
Samuel <65255295+VanStrudel28@users.noreply.github.com>
2022-02-26 18:54:44 -0500

    Update 1.3
    Removed previously added space

--- app/src/main/java/com/bluemethod/paniccalculator/CalculatorFunctionality.java
+++ app/src/main/java/com/bluemethod/paniccalculator/CalculatorFunctionality.java
@@ -124,7 +124,6 @@ public class CalculatorFunctionality{
             evalLine();
         }
         else{
-            setDisplay(getDisplay() + " ");
             setDisplay(getDisplay() + j);
             setSign(j);
             if(sign == "%"){
